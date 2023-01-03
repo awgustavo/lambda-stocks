44114301ea55aa788747ae417d535d3211a081b3
andregustavo-plank <andre.gustavo@joinplank.com>
2023-01-03 17:09:28 -0300

    feat: build script

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { StocksService } from '@application/stocks.service'
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda'
 
-export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`)
     console.log(`Context: ${JSON.stringify(context, null, 2)}`)
     console.log(new StocksService().validate())
