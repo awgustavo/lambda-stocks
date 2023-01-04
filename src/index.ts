6b9c5d6d4a695844235a7a374367143527e37fd1
andregustavo-plank <andre.gustavo@joinplank.com>
2023-01-04 11:38:53 -0300

    feat: add logs and returning error

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { StockDTO } from '@application/stocks/stocks.dto'
-import { StocksModule } from '@application/stocks/stocks.module'
+// import { StockDTO } from '@application/stocks/stocks.dto'
+// import { StocksModule } from '@application/stocks/stocks.module'
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda'
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     console.log('Function started.')
     try {
-        const result = await StocksModule.service.sendStock(JSON.parse(event.body) as StockDTO)
+        const result = {}//await StocksModule.service.sendStock(JSON.parse(event.body) as StockDTO)
         console.log('Message sent.')
         return {
             statusCode: 200,
