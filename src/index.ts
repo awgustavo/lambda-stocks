a13e04205818abbbe706e3b110096988169ac818
andregustavo-plank <andre.gustavo@joinplank.com>
2023-01-04 11:30:02 -0300

    feat: add logs and returning error

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ import { StocksModule } from '@application/stocks/stocks.module'
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda'
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    console.info('Function started.')
+    console.log('Function started.')
     try {
         const result = await StocksModule.service.sendStock(JSON.parse(event.body) as StockDTO)
-        console.info('Message sent.')
+        console.log('Message sent.')
         return {
             statusCode: 200,
             body: JSON.stringify(result)
         }
     }
     catch (error) {
-        console.error(error)
+        console.log(error)
         return {
             statusCode: 500,
             body: JSON.stringify(error)
